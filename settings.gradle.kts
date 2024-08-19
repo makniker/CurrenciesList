710c40e787c60586ca416b16bb01a64db75cacc5
makniker <102602253+makniker@users.noreply.github.com>
2024-08-19 15:35:21 +0300

    create modules, interfaces

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CurrencyList"
+include(":app")
+include(":features:currency")
+include(":core:network")
+include(":core:data")
+include(":core:common-ui")
+
