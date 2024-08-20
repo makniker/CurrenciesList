5debcf6d80f092bc475071fc7875246a5f81f174
makniker <102602253+makniker@users.noreply.github.com>
2024-08-20 08:37:43 +0300

    error screen complete

--- core/common-ui/src/main/java/com/example/core/common_ui/ErrorScreen.kt
+++ core/common-ui/src/main/java/com/example/core/common_ui/ErrorScreen.kt
@@ -3,34 +3,50 @@ package com.example.core.common_ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.core.common_ui.theme.main
 
 @Composable
 fun ErrorScreen(modifier: Modifier = Modifier, onBackButtonPressed: () -> Unit) {
     val painter = painterResource(id = R.drawable.ic_bitcoin_logo_120)
-    Column(modifier = modifier.fillMaxSize(),
+    Column(
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painter, contentDescription = null, modifier = Modifier.size(120.dp)
         )
-        Text(text = stringResource(R.string.error_message),
-            textAlign = TextAlign.Center,)
-        Button(content = {
-            Text(text = stringResource(R.string.try_again)) },
-            onClick = { onBackButtonPressed() })
+        Spacer(Modifier.height(13.dp))
+        Text(
+            text = stringResource(R.string.error_message),
+            textAlign = TextAlign.Center,
+        )
+        Spacer(Modifier.height(30.dp))
+        Button(
+            content = {
+                Text(text = stringResource(R.string.try_again), color = Color.White)
+            },
+            onClick = { onBackButtonPressed() },
+            shape = RoundedCornerShape(4.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = main)
+        )
     }
 }
 
